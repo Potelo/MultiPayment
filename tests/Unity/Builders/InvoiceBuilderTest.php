1133380cc092fd20ef05321c679824cec760e494
Andre Walker <andre.walkermo@gmail.com>
2022-03-09 10:30:09 -0300

    Simplifica teste

--- tests/Unity/Builders/InvoiceBuilderTest.php
+++ tests/Unity/Builders/InvoiceBuilderTest.php
@@ -2,8 +2,8 @@
 
 namespace Potelo\MultiPayment\Tests\Unity\Builders;
 
+use Carbon\Carbon;
 use Potelo\MultiPayment\Tests\TestCase;
-use Potelo\MultiPayment\Exceptions\MultiPaymentException;
 
 class InvoiceBuilderTest extends TestCase
 {
@@ -11,7 +11,7 @@ class InvoiceBuilderTest extends TestCase
     /**
      * Create invoice test.
      *
-     * @dataProvider \Potelo\MultiPayment\Tests\DataProvider::shouldCreateInvoiceDataProvider
+     * @dataProvider shouldCreateInvoiceDataProvider
      *
      * @param  string  $gateway
      * @param  array  $data
@@ -67,5 +67,183 @@ class InvoiceBuilderTest extends TestCase
         $invoice = $invoiceBuilder->create();
         $this->assertInstanceOf(\Potelo\MultiPayment\Models\Invoice::class, $invoice);
         $this->assertObjectHasAttribute('id', $invoice);
+    }
+
+    /**
+     * @return array[]
+     */
+    public function shouldCreateInvoiceDataProvider(): array
+    {
+        return [
+            'iugu - without payment method' => [
+                'gateway' => 'iugu',
+                'data' => [
+                    'expirationDate' => Carbon::now()->addWeekday()->format('Y-m-d'),
+                    'items' => [['description' => 'Teste', 'quantity' => 1, 'price' => 100,],],
+                    'customer' => self::customerWithAddress(),
+                ]
+            ],
+            'moip - without payment method' => [
+                'gateway' => 'moip',
+                'data' => [
+                    'expirationDate' => Carbon::now()->addWeekday()->format('Y-m-d'),
+                    'items' => [['description' => 'Teste', 'quantity' => 1, 'price' => 100,],],
+                    'customer' => self::customerWithAddress(),
+                ]
+            ],
+            'iugu - company with address without payment method' => [
+                'gateway' => 'iugu',
+                'data' => [
+                    'expirationDate' => Carbon::now()->addWeekday()->format('Y-m-d'),
+                    'items' => [['description' => 'Teste', 'quantity' => 1, 'price' => 100,],],
+                    'customer' => self::companyWithAddress(),
+                ]
+            ],
+            'moip - company with address without payment method' => [
+                'gateway' => 'moip',
+                'data' => [
+                    'expirationDate' => Carbon::now()->addWeekday()->format('Y-m-d'),
+                    'items' => [['description' => 'Teste', 'quantity' => 1, 'price' => 100,],],
+                    'customer' => self::companyWithAddress(),
+                ]
+            ],
+            'moip - without payment method and without address' => [
+                'gateway' => 'moip',
+                'data' => [
+                    'expirationDate' => Carbon::now()->addWeekday()->format('Y-m-d'),
+                    'items' => [['description' => 'Teste', 'quantity' => 1, 'price' => 100,],],
+                    'customer' => self::customerWithoutAddress(),
+                ]
+            ],
+            'iugu - credit card without address' => [
+                'gateway' => 'iugu',
+                'data' => [
+                    'items' => [['description' => 'Teste', 'quantity' => 1, 'price' => 100,],],
+                    'customer' => self::customerWithoutAddress(),
+                    'paymentMethod' => 'credit_card',
+                    'creditCard' => self::creditCard(),
+                ]
+            ],
+            'moip - credit card without address' => [
+                'gateway' => 'moip',
+                'data' => [
+                    'items' => [['description' => 'Teste', 'quantity' => 1, 'price' => 100,],],
+                    'customer' => self::customerWithoutAddress(),
+                    'paymentMethod' => 'credit_card',
+                    'creditCard' => self::creditCard(),
+                ]
+            ],
+            'iugu - credit card with address' => [
+                'gateway' => 'iugu',
+                'data' => [
+                    'items' => [['description' => 'Teste', 'quantity' => 1, 'price' => 100,],],
+                    'customer' => self::customerWithAddress(),
+                    'paymentMethod' => 'credit_card',
+                    'creditCard' => self::creditCard(),
+                ]
+            ],
+            'moip - credit card with address' => [
+                'gateway' => 'moip',
+                'data' => [
+                    'items' => [['description' => 'Teste', 'quantity' => 1, 'price' => 100,],],
+                    'customer' => self::customerWithAddress(),
+                    'paymentMethod' => 'credit_card',
+                    'creditCard' => self::creditCard(),
+                ]
+            ],
+            'iugu - bank slip with address' => [
+                'gateway' => 'iugu',
+                'data' => [
+                    'expirationDate' => Carbon::now()->addWeekday()->format('Y-m-d'),
+                    'items' => [['description' => 'Teste', 'quantity' => 1, 'price' => 100,],],
+                    'customer' => self::customerWithAddress(),
+                    'paymentMethod' => 'bank_slip',
+                ]
+            ],
+            'moip - bank slip with address' => [
+                'gateway' => 'moip',
+                'data' => [
+                    'expirationDate' => Carbon::now()->addWeekday()->format('Y-m-d'),
+                    'items' => [['description' => 'Teste', 'quantity' => 1, 'price' => 100,],],
+                    'customer' => self::customerWithAddress(),
+                    'paymentMethod' => 'bank_slip',
+                ]
+            ],
+            'iugu - pix with address' => [
+                'gateway' => 'iugu',
+                'data' => [
+                    'expirationDate' => Carbon::now()->addWeekday()->format('Y-m-d'),
+                    'items' => [['description' => 'Teste', 'quantity' => 1, 'price' => 100,],],
+                    'customer' => self::customerWithAddress(),
+                    'paymentMethod' => 'pix',
+                ]
+            ],
+            'iugu - pix without address' => [
+                'gateway' => 'iugu',
+                'data' => [
+                    'expirationDate' => Carbon::now()->addWeekday()->format('Y-m-d'),
+                    'items' => [['description' => 'Teste', 'quantity' => 1, 'price' => 100,],],
+                    'customer' => self::customerWithoutAddress(),
+                    'paymentMethod' => 'pix',
+                ]
+            ],
+        ];
+    }
+
+    public static function customerWithoutAddress(): array
+    {
+        $customer['name'] = 'Fake Customer';
+        $customer['email'] = 'email@exemplo.com';
+        $customer['taxDocument'] = '20176996915';
+        $customer['birthDate'] = '1980-01-01';
+        $customer['phoneArea'] = '71';
+        $customer['phoneNumber'] = '982345678';
+        return $customer;
+    }
+
+    public static function companyWithAddress(): array
+    {
+        $customer = self::customerWithoutAddress();
+        $customer['address'] = self::address();
+        return $customer;
+    }
+
+    public static function companyWithoutAddress(): array
+    {
+        $customer['name'] = 'Fake Company';
+        $customer['email'] = 'email@exemplo.com';
+        $customer['taxDocument'] = '28585583000189';
+        return $customer;
+    }
+
+    public static function customerWithAddress(): array
+    {
+        $customer = self::customerWithoutAddress();
+        $customer['address'] = self::address();
+        return $customer;
+    }
+
+    public static function address(): array
+    {
+        $address['zipCode'] = '41820330';
+        $address['street'] = 'Rua Exemplo';
+        $address['number'] = '123';
+        $address['district'] = 'Bairro Exemplo';
+        $address['complement'] = 'Apto. 123';
+        $address['city'] = 'Cidade Exemplo';
+        $address['state'] = 'Estado';
+        $address['country'] = 'Brasil';
+        return $address;
+    }
+
+    public static function creditCard(): array
+    {
+        $creditCard['number'] = '4111111111111111';
+        $creditCard['month'] = '12';
+        $creditCard['year'] = \Carbon\Carbon::now()->addYear()->format('Y');
+        $creditCard['cvv'] = '123';
+        $creditCard['firstName'] = 'Faker';
+        $creditCard['lastName'] = 'Teste';
+        return $creditCard;
     }
 }
