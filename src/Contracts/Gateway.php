c7510b8e043c33d6491e4d691c6f9c4e2975e6f8
Andre Walker <andre.walkermo@gmail.com>
2022-11-08 21:03:20 -0300

    refactor: fix: adiciona assinatura do método `createCreditCard`

--- src/Contracts/Gateway.php
+++ src/Contracts/Gateway.php
@@ -4,6 +4,7 @@ namespace  Potelo\MultiPayment\Contracts;
 
 use Potelo\MultiPayment\Models\Invoice;
 use Potelo\MultiPayment\Models\Customer;
+use Potelo\MultiPayment\Models\CreditCard;
 use Potelo\MultiPayment\Exceptions\GatewayException;
 use Potelo\MultiPayment\Exceptions\GatewayNotAvailableException;
 
@@ -39,5 +40,14 @@ interface Gateway
      * @throws GatewayException|GatewayNotAvailableException
      */
     public function getInvoice(string $id): Invoice;
-    
+
+    /**
+     * Create a credit card
+     *
+     * @param CreditCard $creditCard
+     *
+     * @return CreditCard
+     * @throws GatewayException|GatewayNotAvailableException
+     */
+    public function createCreditCard(CreditCard $creditCard): CreditCard;
 }
