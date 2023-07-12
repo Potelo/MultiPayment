1003529e81a38499272cc734e84bd509d3b6e325
root <root@GUI>
2023-07-12 12:51:34 -0300

    invoiceCustomVariable class

---
+++ src/Models/InvoiceCustomVariable.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Potelo\MultiPayment\Models;
+
+/**
+ * Class InvoiceCustomVariable
+ */
+class InvoiceCustomVariable extends Model
+{
+    /**
+     * @var string
+     */
+    public string $name;
+
+    /**
+     * @var string
+     */
+    public string $value;
+}
