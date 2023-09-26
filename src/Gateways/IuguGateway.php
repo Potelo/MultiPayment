d444b53254ca05c02f0d498dcc48271232a5865a
Andre Walker <andre.walkermo@gmail.com>
2023-09-26 12:23:56 -0300

    fix: adiciona invoice_id novamente

--- src/Gateways/IuguGateway.php
+++ src/Gateways/IuguGateway.php
@@ -112,6 +112,7 @@ class IuguGateway implements Gateway
                 $invoice->creditCard = $this->createCreditCard($invoice->creditCard);
             }
             $iuguInvoiceData['customer_payment_method_id'] = $invoice->creditCard->id;
+            $iuguInvoiceData['invoice_id'] = $iuguInvoice->id;
             unset($iuguInvoiceData['items']);
 
             try {
